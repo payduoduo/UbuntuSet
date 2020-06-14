e65c598f6f5df8e3cc487d76dc052732a84cffb7
linyongyao_120911 <342481947@qq.com>
2020-06-14 22:06:11 +0800

    修改

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 sudo apt install git
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
+
+
+
+<!-- 参考 -->
+https://blog.csdn.net/weixin_42053361/article/details/83151252
+https://www.cnblogs.com/sitoi/p/11819633.html
--- install.sh
+++ install.sh
@@ -21,8 +21,10 @@ echo '安装git'
 sudo apt install -y git
 echo '安装zsh和oh_my_zsh'
 sudo apt install -y zsh
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 sudo chsh -s /bin/zsh
+sudo echo 'source ~/.profile' >> ~/.zshrc
+
 echo '安装vim'
 sudo apt install -y vim
 echo '安装mysql'
@@ -38,6 +40,8 @@ sudo apt install -y mysql-server
 echo '安装gnome'
 sudo apt install -y gnome-tweak-tool
 sudo apt install -y gnome-shell-extensions
+echo '安装dash-to-dock'
+sudo apt install -y gnome-shell-extension-dashtodock
 echo '安装dash-to-panel'
 sudo apt install -y gnome-shell-extension-dash-to-panel
 echo '安装chrome'
@@ -45,9 +49,38 @@ sudo wget http://www.linuxidc.com/files/repo/google-chrome.list -P /etc/apt/sour
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y google-chrome-stable
-echo '安装Typora'
+echo '安装Typora(速度很慢)'
 wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
 sudo add-apt-repository 'deb https://typora.io/linux ./'
 sudo apt-get update
 sudo apt-get install -y typora
 echo '安装vscode(没处理)'
+
+echo '安装vim'
+sudo apt install vim
+echo '安装进程监控工具htop'
+sudo apt install htop
+
+
+echo '安装py3 pip'
+sudo apt install python3 python3-pip
+echo '更换pipy源'
+sudo mkdir ~/.pip
+sudo cp ./pip.conf ~/.pip/
+
+echo '安装curl'
+sudo apt install curl
+echo '安装网络工具,ifconfig'
+sudo apt install net-tools
+
+
+echo '生成本机密钥'
+ssh-keygen -t rsa -C "payduoduo@163.com"
+echo "默认路径为 ~/.ssh/"
+# sudo cat ~/.ssh/
+
+
+echo '安装docker'
+sudo apt install docker.io
+echo '更换docker源(未测试)'
+sudo cat ./daemon.json > /etc/docker/daemon.json
